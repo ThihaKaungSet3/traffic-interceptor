b4568b77492a1993f01c232638a5a418aff9bdc8
thihakaungsett <thihakaungsett@rezeve.com>
2023-04-24 08:39:07 +0630

    Updated Random Country with Interval

--- vendors/common.go
+++ vendors/common.go
@@ -3,4 +3,5 @@ package vendors
 type ProxyConfig struct {
 	IP   string
 	Port int
+	Code string
 }
--- vendors/proxyscrape.go
+++ vendors/proxyscrape.go
@@ -2,13 +2,17 @@ package vendors
 
 import (
 	"bufio"
+	"math/rand"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func GetProxyScrapeFreemium() ([]ProxyConfig, error) {
-	res, err := http.Get("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=1000&country=us,gb&ssl=all&anonymity=all")
+	country := getRandomCountry()
+	url := "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=1000&country=" + country.Code + "&ssl=all&anonymity=all"
+	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -31,8 +35,26 @@ func GetProxyScrapeFreemium() ([]ProxyConfig, error) {
 		config := ProxyConfig{
 			IP:   ipAddress,
 			Port: port,
+			Code: country.Code,
 		}
 		available = append(available, config)
 	}
 	return available, nil
+}
+
+func getRandomCountry() country {
+	rand.Seed(time.Now().UnixNano())
+	countries := []country{
+		{Name: "US", Code: "us"},
+		{Name: "Canada", Code: "ca"},
+		{Name: "Iran", Code: "ir"},
+		{Name: "Norway,Netherland,Czech,Japan", Code: "no,nl,cz,jp"},
+	}
+	randomIndex := rand.Intn(len(countries))
+	return countries[randomIndex]
+}
+
+type country struct {
+	Name string
+	Code string
 }

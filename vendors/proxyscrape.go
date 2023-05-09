f58b44668bbb3de99b6182e50cb47c297190439f
thihakaungsett <thihakaungsett@rezeve.com>
2023-05-09 09:17:19 +0630

    Updated de

--- vendors/proxyscrape.go
+++ vendors/proxyscrape.go
@@ -46,7 +46,7 @@ func GetProxyScrapeFreemium() ([]ProxyConfig, error) {
 func GetRandomCountry() country {
 	countries := []country{
 		// us,ca,cz,nl
-		{Name: "Globle", Code: "us,ca,cz,nl,gb,sg,ir"},
+		{Name: "Globle", Code: "us,cz,nl,de"},
 		// {Name: "Europe", Code: "cz,nl"},
 	}
 	rand.Seed(time.Now().UnixNano())
